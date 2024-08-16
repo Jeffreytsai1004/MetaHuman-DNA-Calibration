4f1ca2127674322f7de2e843fbc70fef3935a2fa
Jeffreytsai1004 <109943015+Jeffreytsai1004@users.noreply.github.com>
2024-08-16 13:30:05 +0800

    Update util.py

--- dna_viewer/builder/maya/util.py
+++ dna_viewer/builder/maya/util.py
@@ -13,19 +13,11 @@ from ...common import DNAViewerError
 
 
 class Maya:
-    """A utility class used for interfacing with maya transforms"""
+    """用于与maya变换接口的实用程序类"""
 
     @staticmethod
     def get_element(name: str) -> Union[MDagPath, MFnDagNode]:
         """
-        gets the Union[MDagPath, MFnDagNode] object of the element with the given name
-
-        @type name: str
-        @param name: The name of the element to be retrieved
-
-        @rtype: Union[MDagPath, MFnDagNode]
-        @returns: A OpenMaya object representing the given element
-
         获取具有给定名称的元素的 Union[MDagPath, MFnDagNode] 对象
 
         @type name: str
@@ -47,14 +39,6 @@ class Maya:
     @staticmethod
     def get_transform(name: str) -> MFnTransform:
         """
-        gets the transform of the element with the given name
-
-        @type element: str
-        @param element: The element name that we want the transform of
-
-        @rtype: MFnTransform
-        @returns: A MFnTransform object representing the given elements transform
-
         获取具有给定名称的元素的变换
         
         @type element: str
@@ -68,17 +52,6 @@ class Maya:
     @staticmethod
     def get_translation(element: str, space: int = MSpace.kObject) -> MVector:
         """
-        gets the translation of the element with the given name
-
-        @type element: str
-        @param element: The element name that we want the translation of
-
-        @type space: str
-        @param space: A string value representing the translation space (default is "world")
-
-        @rtype: MVector
-        @returns: A MVector object representing the given elements translation
-
         获取具有给定名称的元素的翻译
         
         @type element: str
@@ -97,17 +70,6 @@ class Maya:
         element: str, translation: MVector, space: int = MSpace.kObject
     ) -> None:
         """
-        sets the translation of the element with the given name
-
-        @type element: str
-        @param element: The element name that we want to set the translation of
-
-        @type translation: MVector
-        @param translation: The new translation value
-
-        @type space: str
-        @param space: A string value representing the translation space (default is "object")
-
         设置具有给定名称的元素的翻译
 
         @type元素：str
