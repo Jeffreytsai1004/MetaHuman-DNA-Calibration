bd766843011d5d6fff8ec3ce55c8cced9530f8fa
Jeffreytsai1004 <109943015+Jeffreytsai1004@users.noreply.github.com>
2024-08-16 16:13:55 +0800

    Update dnacalib_lod_demo.py

--- examples/dnacalib_lod_demo.py
+++ examples/dnacalib_lod_demo.py
@@ -1,22 +1,21 @@
 """
-This example demonstrates creation of a new DNA from an existing one by extracting specific lods.
-IMPORTANT: You have to setup the environment before running this example. Please refer to the 'Environment setup' section in README.md.
+此示例演示了通过从现有DNA中提取特定LOD来创建新的DNA。
+重要提示：在运行此示例之前，您必须设置环境。请参考 README.md 中的“环境设置”部分。
 
-- usage in command line:
+- 在命令行中使用：
     python dnacalib_lod_demo.py
     mayapy dnacalib_lod_demo.py
 
-- usage in Maya:
-    1. copy whole content of this file to Maya Script Editor
-    2. change value of ROOT_DIR to absolute path of dna_calibration, e.g. `c:/dna_calibration` in Windows or `/home/user/dna_calibration`. Important:
-    Use `/` (forward slash), because Maya uses forward slashes in path.
+- 在Maya中使用：
+    1.将此文件的整个内容复制到Maya脚本编辑器中
+    2.将ROOT_DIR的值更改为dna_calibration的绝对路径，例如在Windows中为`c:/dna_calibration`或在`/home/user/dna_calibration`。重要提示：在路径中使用`/`（正斜杠），因为Maya在路径中使用正斜杠。
 
-- customization:
-    - change CHARACTER_NAME to Taro, or the name of a custom DNA file placed in /data/dna_files
-    - change value of LODS to list of lods needed to be extracted
+- 自定义：
+    -将CHARACTER_NAME更改为Taro，或将其更改为放置在/data/dna_files中的自定义DNA文件的名称
+    -将LODS的值更改为需要提取的LOD列表
 
-Expected: Script will generate Ada_with_lods_1_and_3.dna in OUTPUT_DIR, from original Ada.
-NOTE: If OUTPUT_DIR does not exist, it will be created.
+预期结果：脚本将从原始Ada生成Ada_with_lods_1_and_3.dna放在OUTPUT_DIR中。
+注意：如果OUTPUT_DIR不存在，将会创建它。
 """
 
 
