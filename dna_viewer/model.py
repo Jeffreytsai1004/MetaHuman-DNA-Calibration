bc30202cec41ea9395e985b77fb38c1ccf7e4fd0
Jeffreytsai1004 <109943015+Jeffreytsai1004@users.noreply.github.com>
2024-08-16 16:03:38 +0800

    Update model.py

--- dna_viewer/model.py
+++ dna_viewer/model.py
@@ -5,18 +5,18 @@ from typing import Dict, List
 @dataclass
 class Point3:
     """
-    A model class for representing a 3 dimensional point
-
-    Attributes
+    为表示三维点的模型类
+    
+    属性
     ----------
     @type x: float
-    @param x: The value of x
-
+    @param x: x的值
+    
     @type y: float
-    @param y: The value of y
-
+    @param y: y的值
+    
     @type z: float
-    @param z: The value of z
+    @param z: z的值
     """
 
     x: float = field(default=0.0)
@@ -27,15 +27,15 @@ class Point3:
 @dataclass
 class UV:
     """
-    A model class for holding data about the UV
-
-    Attributes
+    一个用于保存有关UV数据的模型类
+    
+    属性
     ----------
-    @type u: float
-    @param u: The value of u
-
-    @type v: float
-    @param v: The value of v
+    @type u: 浮点数
+    @param u: u的值
+    
+    @type v: 浮点数
+    @param v: v的值
     """
 
     u: float = field(default=0.0)
@@ -45,15 +45,15 @@ class UV:
 @dataclass
 class Layout:
     """
-    A model class for holding data about a single layout
-
-    Attributes
+    为保存有关单个布局数据的模型类
+    
+    属性
     ----------
-    @type position_index: int
-    @param position_index: An index representing position
-
-    @type texture_coordinate_index: int
-    @param texture_coordinate_index: A value representing the texture coordinate index
+    @type position_index：int
+    @param position_index：表示位置的索引
+    
+    @type texture_coordinate_index：int
+    @param texture_coordinate_index：表示纹理坐标索引
     """
 
     position_index: int = field(default=0)
@@ -63,21 +63,21 @@ class Layout:
 @dataclass
 class Topology:
     """
-    A model class for holding data about the topology
-
-    Attributes
+    一个用于保存拓扑数据的模型类
+    
+    属性
     ----------
     @type positions: List[Point3]
-    @param positions: List of points in space representing the positions
-
+    @param positions: 空间中位置的点列表
+    
     @type texture_coordinates: List[UV]
-    @param texture_coordinates: List of UVs representing the positions
-
+    @param texture_coordinates: 代表位置的UV列表
+    
     @type layouts: List[Layout]
-    @param layouts: The list of Layout mappings
-
+    @param layouts: 布局映射的列表
+    
     @type face_vertex_layouts: List[List[int]]
-    @param face_vertex_layouts: List of face vertex layout indices by face index
+    @param face_vertex_layouts: 按面索引分类的面顶点布局索引列表
     """
 
     positions: List[Point3] = field(default_factory=list)
@@ -89,15 +89,15 @@ class Topology:
 @dataclass
 class BlendShape:
     """
-    A model class for holding data about the blend shape
-
-    Attributes
+    用于保存混合形状数据的模型类
+    
+    属性
     ----------
     @type channel: int
-    @param channel: The index pointing to the blend shape name
-
+    @param channel: 指向混合形状名称的索引
+    
     @type deltas: Dict[int, Point3]
-    @param deltas: A mapping of blend shape indices to the coordinate differences that are made by the blend shape
+    @param deltas: 将混合形状索引映射到混合形状产生的坐标差异的映射
     """
 
     channel: int = field(default=None)
@@ -107,16 +107,16 @@ class BlendShape:
 @dataclass
 class SkinWeightsData:
     """
-    A model class for holding data about the skin weights
-
-    Attributes
+    一个用于保存有关皮肤权重数据的模型类
+    
+    属性
     ----------
-
+    
     @type values: List[List[float]]
-    @param values: The skin weight values per vertex index
-
+    @param values: 每个顶点索引的皮肤权重值
+    
     @type joint_indices: List[List[int]]
-    @param joint_indices: The joint indces per vertex index
+    @param joint_indices: 每个顶点索引的关节索引
     """
 
     values: List[List[float]] = field(default_factory=list)
@@ -126,21 +126,21 @@ class SkinWeightsData:
 @dataclass
 class Mesh:
     """
-    A model class for holding data about the mesh
-
-    Attributes
+    用于保存网格数据的模型类
+    
+    属性
     ----------
     @type name: str
-    @param name: The name of the mesh
-
+    @param name: 网格的名称
+    
     @type topology: Topology
-    @param topology: Data containing the topology of the mesh
-
+    @param topology: 包含网格拓扑的数据
+    
     @type skin_weights: SkinWeightsData
-    @param skin_weights: Data representing skin weights
-
+    @param skin_weights: 表示皮肤权重的数据
+    
     @type blend_shapes: List[BlendShape]
-    @param blend_shapes: The list of blend shapes for the mesh
+    @param blend_shapes: 网格的混合形状列表
     """
 
     name: str = field(default=None)
@@ -152,21 +152,21 @@ class Mesh:
 @dataclass
 class Joint:
     """
-    A model class for holding data about a single joint
-
-    Attributes
+    一个用于保存单个关节数据的模型类
+    
+    属性
     ----------
     @type name: str
-    @param name: The name of the joint
-
+    @param name: 关节的名称
+    
     @type translation: Point3
-    @param translation: A point in 3 dimensional space which represents the translation of the joint
-
+    @param translation: 代表关节平移的三维空间中的点
+    
     @type orientation: Point3
-    @param orientation: A point in 3 dimensional space which represents the orientation of the joint
-
+    @param orientation: 代表关节方向的三维空间中的点
+    
     @type parent_name: str
-    @param parent_name: The name of the parent joint
+    @param parent_name: 父关节的名称
     """
 
     name: str = field(default=None)
